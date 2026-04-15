9ef52b1f40c4a11b14d396af8162f5357a4c9699
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-04-15 14:44:10 -0500

    Complete exercise 1.3

--- part1/courseInfo/courseInfo/src/App.jsx
+++ part1/courseInfo/courseInfo/src/App.jsx
@@ -4,45 +4,70 @@
 
 
 const App = () => {
-  const Header = (props) => {
-    return (
-      <div>
-        <h1>{props.header}</h1>
-      </div>
-    )
+
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
   }
-  const Content = (props) => {
-    return (
-      <div>
-      <p>
-        {props.title1} has {props.number1} exercises. 
-      </p>
-        <p>
-        {props.title2} has {props.number2} exercises. 
-      </p>
-        <p>
-        {props.title3} has {props.number3} exercises. 
-      </p>
-      </div>
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  }
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  }
+
+  const total = part1.exercises + part2.exercises + part3.exercises;
 
 
-    )
-  }
-  const Total = (props) => {
-    return (
-      <p>
-        Number of exercises is {props.first + props.second + props.third}.
-      </p>
-    )
-  }
 
   return (
     <div>
-      <Header header="Half Stack application development" />
-      <Content title1="Fundementals of Reacts" number1={10} title2="Using props to pass data" number2={7} title3="State of a component" number3={14}/>
-      <Total first={10} second={7} third={14} />
+      <h1>{course}</h1>
+      <p>{part1.name} has {part1.exercises} exercises.</p>
+      <p>{part2.name} has {part2.exercises} exercises.</p>
+      <p>{part3.name} has {part3.exercises} exercises.</p>
+      <p>The total exercises is {total}</p>
+      {/* <Content title1="Fundementals of Reacts" number1={10} title2="Using props to pass data" number2={7} title3="State of a component" number3={14}/>
+      <Total first={10} second={7} third={14} /> */}
     </div>
   )
 }
 
 export default App
+
+
+
+  // const Header = (props) => {
+  //   return (
+  //     <div>
+  //       <h1>{props.header}</h1>
+  //     </div>
+  //   )
+  // }
+  // const Content = (props) => {
+  //   return (
+  //     <div>
+  //     <p>
+  //       {props.title1} has {props.number1} exercises. 
+  //     </p>
+  //       <p>
+  //       {props.title2} has {props.number2} exercises. 
+  //     </p>
+  //       <p>
+  //       {props.title3} has {props.number3} exercises. 
+  //     </p>
+  //     </div>
+
+
+  //   )
+  // }
+  // const Total = (props) => {
+  //   return (
+  //     <p>
+  //       Number of exercises is {props.first + props.second + props.third}.
+  //     </p>
+  //   )
+  // }
