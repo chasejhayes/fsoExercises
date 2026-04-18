7d148e6a77fc9fba3ab43d634dc7e08310e0d28d
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-04-18 11:53:21 -0500

    Complete problem 1.10

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -6,18 +6,19 @@ const App = () => {
     good: 0, neutral: 0, bad: 0
   })
 
-  const[allReviews, setAll] = useState([])
+  const [allReviews, setAll] = useState([])
 
-  
 
-  const Title = ({text}) => <h1>{text}</h1>
+
+  const Title = ({ text }) => <h1>{text}</h1>
 
   const handleGoodReviews = () => {
     const goodClicks = {
       ...reviews,
-      good: reviews.good +1
+      good: reviews.good + 1
     }
     setAll(allReviews.concat("+"))
+    console.log(allReviews)
     setReviews(goodClicks)
   }
 
@@ -39,12 +40,12 @@ const App = () => {
     setReviews(neutralClicks)
   }
 
-  const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
   // const Statistics = ({reviews, text}) => <p>{text} {reviews}</p>
 
-  const AskForSubmit = ({text, arr}) => {
-    if (arr.length ===0){
+  const AskForSubmit = ({ text, arr }) => {
+    if (arr.length === 0) {
       return (
         <p>{text}</p>
       )
@@ -52,37 +53,48 @@ const App = () => {
     return <p></p>
   }
 
-  const Statistics = ({reviews, text, arr}) => {
-    if (arr.length===0){
-      return(
-        <div>
-        </div>
-      )
-    }
-    else {return (<p>{text} {reviews}</p>)}
 
+
+    const StatisticsLine = ({ text, reviews }) => {
+  return <p>{text} {reviews}</p>
   }
 
+
+ 
+
+  const Statistics = ({ display }) => {
+    if (display.length === 0) {
+      return (
+        <div></div>
+      )
+    }
+    else {
+      return (
+          <div>
+            <StatisticsLine text="good" reviews={reviews.good}/>
+            <StatisticsLine text="neutral" reviews={reviews.neutral}/>
+            <StatisticsLine text="bad" reviews={reviews.bad}/>
+            <StatisticsLine text="total" reviews={reviews.bad + reviews.good + reviews.neutral}/>
+            <StatisticsLine  text="average" reviews={((reviews.good * 1) + (reviews.bad * -1) + (reviews.neutral * 0)) / (reviews.bad + reviews.good + reviews.neutral)}/>
+            <StatisticsLine text="positive" reviews={(reviews.good / (reviews.bad + reviews.good + reviews.neutral)) * 100 + "%"} />
+          </div>
+      )
+    }
+  }
  
 
 
 
   return (
     <div>
-      <Title text="Give Feedback"/>
-      <Button onClick = {handleGoodReviews} text = {"Add Good"}/>
-      <Button onClick = {handleBadReviews} text={"Add Bad"}/>
-      <Button onClick = {handleNeutralReviews} text={"Add Neutral"}/>
-    
-      <Title text="Statistics" />
-      <AskForSubmit text= "Please submit a review" arr={allReviews}/>
+      <Title text="Give Feedback" />
+      <Button onClick={handleGoodReviews} text={"Add Good"} />
+      <Button onClick={handleBadReviews} text={"Add Bad"} />
+      <Button onClick={handleNeutralReviews} text={"Add Neutral"} />
 
-      <Statistics text={"good"} reviews={reviews.good} arr={allReviews}/>
-      <Statistics text={"neutral"} reviews={reviews.neutral} arr={allReviews}/>
-      <Statistics text={"bad"} reviews={reviews.bad} arr={allReviews}/>
-      <Statistics text={"total"} reviews={reviews.bad + reviews.good + reviews.neutral} arr={allReviews}/>
-      <Statistics text={"average"} reviews={((reviews.good*1)+(reviews.bad*-1)+(reviews.neutral*0))/(reviews.bad + reviews.good + reviews.neutral)} arr={allReviews}/>
-      <Statistics text={"positive"} reviews={(reviews.good/(reviews.bad + reviews.good + reviews.neutral))*100+"%"} arr={allReviews}/>
+      <Title text="Statistics" />
+      <AskForSubmit text="Please submit a review" arr={allReviews} />
+      <Statistics display={allReviews} />
     </div>
   )
 }
