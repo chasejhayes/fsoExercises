dfbdb6cd6be6415a85f11075c143c78b02704c75
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-04-22 12:52:43 -0500

    Complete 2.11 - add json server and effect hook

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,116 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axis from "axios"
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: "Arto Hellas"
-    },
-    { name: "Chase" }
   ])
 
-  const [newName, setNewName] = useState("")
-  const [newNumber, setNewNumber] = useState("")
-  const [filter, setFilter] = useState([])
+const [newName, setNewName] = useState("")
+const [newNumber, setNewNumber] = useState("")
+
+const hook = () => {
+  axios
+  .get('http://localhost:3002/persons')
+  .then(response => {
+    setPersons(response.data)
+  })
+}
+
+useEffect(hook, [])
 
 
-
-
-  const addName = (event) => {
-    event.preventDefault()
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-    }
-    if (persons.some(person => person.name === nameObject.name)) {
-      return alert(`${newName} is already in the phone book.`)
-    } else {
-      setPersons(persons.concat(nameObject))
-      setNewName("")
-      setNewNumber("")
-    }
-    return nameObject
+const addName = (event) => {
+  event.preventDefault()
+  const nameObject = {
+    name: newName,
+    number: newNumber,
   }
-
-
-
-  const filterResults = (event) => {
-    event.preventDefault()
-    const filterArr = filter.split("");
-    let splitNameArr = persons.map(person => person.name.split(""))
-
-    function startsWith(arr, prefix){
-      for( let i=0; i<arr.length; i++){
-        if (arr[i] !== prefix) 
-        console.log(arr)
-        console.log(prefix)
-        console.log(false)
-      }
-      console.log(arr)
-      console.log(prefix)
-      console.log(true)
-      setFilter(prefix)
-    }
-
-    startsWith(splitNameArr, filterArr)
-   
-    console.log(filter)
-    console.log(filterArr)
-    console.log(persons)
-    
-    setFilter("")
+  if (persons.some(person=> person.name === nameObject.name)){
+    return alert(`${newName} is already in the phone book.`)
+  } else {
+  setPersons(persons.concat(nameObject))
+  setNewName("")
+  setNewNumber("")
+  
   }
+}
 
+const handleNameChange = (event) => {
+  setNewName(event.target.value)
+}
+const handleNumberChange = (event) => {
+  setNewNumber(event.target.value)
+}
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+function Form ({value, text}){
+  <div>{text} {value}</div>
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
+}
 
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+return (
+  <div>
+    <h2>Phonebook</h2>
+    <form onSubmit={addName}>
+      <Form text="Name1: " value={newNumber}/>
       <div>
-        {persons.map(person =>
-          <div key={person.name}>
-            {person.name} {person.number}
-          </div>
+        name: <input  value={newName} onChange={handleNameChange}/>
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+    <h2>Numbers</h2>
+    <div>
+      {persons.map(person=>
+      <div key={person.name}>
+        {person.name} {person.number}
+        </div>
         )}
       </div>
-      <div>
-        <form onSubmit={filterResults}>
-          <div>
-            filter: <input value={filter} onChange={handleFilterChange} />
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-          <div>{filter}</div>
-        </form>
-
-        <div>debug: {newName}</div>
-        <div>debug: {filter}</div>
-      </div>
-    </div>
-  )
+    <div>debug: {newName}</div>
+  </div>
+  
+)
 
 }
 
