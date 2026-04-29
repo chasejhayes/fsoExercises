f0617388bf622cb1a3e20ef72908b146722b6aaa
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-04-29 13:10:43 -0500

    Complete fso exercise 3.3

--- part3/index.js
+++ part3/index.js
@@ -44,6 +44,18 @@ app.get("/info", (request, response)=> {
   
 })
 
+app.get("/api/people/:id", (request, response)=> {
+    const id = request.params.id
+    const person = people.find(person => person.id === id)
+    
+   
+    if(person){
+          response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 
 const PORT = 3001
 app.listen(PORT, ()=> {
